e30406e0a3e5018bfbae28334dfa0c122ce373ac
Major9999 <momohamed2157@gmail.com>
2023-06-06 23:04:12 +0000

    The Sorted L List

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - Func Insert in sorted linked list.
+ * @head: The Head pointer to the list.
+ * @number: The integer inserted.
+ * Return: Just SOme Value.
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+
+	{
+		return (NULL);
+	}
+
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+
+	{
+		node = node->next;
+	}
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
